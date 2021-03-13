b8ed20e8836dbe639720a15f7d3abbb142186201
giuseppecolucci <giuseppecolucciwork@gmail.com>
2021-03-13 17:14:21 +0100

    struttura home completata

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,5 @@
 <?php
-
 $array=config('pasta');
-
 $contatore=0;
 ?>
 <!DOCTYPE html>
@@ -10,20 +8,62 @@ $contatore=0;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
+<div class="header">
     @include('header')
-    
-@foreach($array as $key=>$val)
-<a href="/product/{{$contatore}}">
-<h1>{{$val['titolo']}}</h1>
-<img src="{{$val['src']}}" alt="">
-</a>
-@php
-$contatore++
-@endphp
-@endforeach
-@include('header')
+</div>
+  <div class="container-box">
+   <h2>lunghe</h2>
+    <div class="cards">
+      @foreach ($array as $key =>$val)
+      <div class="box-cards">
+        @if ($val['tipo'] === 'lunga')
+          <a href="/product/{{$key}}">
+          <img src="{{$val['src']}}" alt="">
+        </a>
+        @endif
+      </div>
+        @endforeach
+    </div>
+  </div>
+  <div class="container-box">
+   <h2>le corte</h2>
+    <div class="cards">
+      @foreach ($array as $key =>$val)
+      <div class="box-cards">
+        @if ($val['tipo'] === 'corta')
+          <a href="/product/{{$key}}">
+          <img src="{{$val['src']}}" alt="">
+        </a>
+        @endif
+      </div>
+        @endforeach
+    </div>
+  </div>
+  <div class="container-box">
+   <h2>le cortissime</h2>
+    <div class="cards">
+      @foreach ($array as $key =>$val)
+      <div class="box-cards">
+        @if ($val['tipo'] === 'cortissima')
+          <a href="/product/{{$key}}">
+          <img src="{{$val['src']}}" alt="">
+        </a>
+        @endif
+      </div>
+        @endforeach
+    </div>
+  </div>
+
+
+ 
+ 
+  </div> 
+  <div class="footer">
+    @include('header')
+  </div> 
 </body>
 </html>
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,6 +1,5 @@
 <?php
 $array=config('pasta');
-//variabile di collegamento all array
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -8,14 +7,19 @@ $array=config('pasta');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>Document</title>
 </head>
 <body>
-@include('header')
+<div class="header">
+    @include('header')
+</div>
 <h3>{{$array[$id]['titolo']}}</h3>
 <img src="{{$array[$id]['src-h']}}" alt="">
 <img src="{{$array[$id]['src-p']}}" alt="">
 <p>{{$array[$id]['descrizione']}}</p>
-@include('footer') 
+<div class="footer">
+    @include('header')
+  </div> 
 </body>
 </html>
